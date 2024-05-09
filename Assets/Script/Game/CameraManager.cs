f0d7a9e2b5864553f38b3e6dcbc659a5cebd4704
hemerick <hemericklecuyer04@gmail.com>
2024-05-08 22:07:47 -0400

    BUG FIXED - PERSPECTIVE
    - Fixed a Camera BUG that caused static object to move (Perspective BUG)
    - Set the camera to "Orthographic" and it fixed all the visual bug

--- Assets/Script/Game/CameraManager.cs
+++ Assets/Script/Game/CameraManager.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     GameObject target;
-    float moveSpeed = 0.75f;
+    float moveSpeed = 1.1f;
     void Start()
     {
         target = Player.GetInstance().gameObject;
         transform.position = new Vector3(SpawnPoint.GetInstance().transform.position.x, SpawnPoint.GetInstance().transform.position.y, SpawnPoint.GetInstance().transform.position.z - 5);
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
         FollowTarget();
     }
@@ -23,7 +23,7 @@ public class CameraManager : MonoBehaviour
         if (target != null) 
         {
             Vector3 FinalPosition = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z - 10);
-            Vector3 transitionPosition = Vector3.Lerp(transform.position, FinalPosition, moveSpeed * Time.fixedDeltaTime);
+            Vector3 transitionPosition = Vector3.Lerp(transform.position, FinalPosition, moveSpeed * Time.deltaTime);
             transform.position = transitionPosition;
         }
     }
